03913b69452e5cf946d78b75f47d22718baf3650
Santhosh-004 <133385174+Santhosh-004@users.noreply.github.com>
2023-09-28 17:02:28 +0530

    init

--- index.js
+++ index.js
@@ -1,39 +1,143 @@
-document.querySelector("#productName").textContent = "realme narzo 60 5G (Mars Orange,8GB+128GB)";
-document.querySelector("#productImage").src = "https://m.media-amazon.com/images/I/8195A49fZbL._SL1500_.jpg"
+let cUrl;
 
-document.querySelector("#productList1").addEventListener("change", function(){
-    if (this.value == "volvo"){
-        document.querySelector("#productPrice1").textContent = 10000;
-    }
-    else if (this.value == "saab"){
-        document.querySelector("#productPrice1").textContent = 20000;
-    }
-    else if (this.value == "mercedes"){
-        document.querySelector("#productPrice1").textContent = 30000;
-    }
-});
+async function getTabUrl() {
+   return new Promise((resolve) => {
+      chrome.tabs.query({ currentWindow: true, active: true }, async (tabs) => {
+         cUrl = await tabs[0].url;
+         resolve();
+      });
+   });
+}
 
-document.querySelector("#productList2").addEventListener("change", function(){
-    if (this.value == "volvo"){
-        document.querySelector("#productPrice2").textContent = 10000;
-    }
-    else if (this.value == "saab"){
-        document.querySelector("#productPrice2").textContent = 20000;
-    }
-    else if (this.value == "mercedes"){
-        document.querySelector("#productPrice2").textContent = 30000;
-    }
-});
+async function main() {
+   await getTabUrl();
+   console.log(cUrl);
 
-document.querySelector("#productList3").addEventListener("change", function(){
-    if (this.value == "volvo"){
-        document.querySelector("#productPrice3").textContent = 10000;
-    }
-    else if (this.value == "saab"){
-        document.querySelector("#productPrice3").textContent = 20000;
-    }
-    else if (this.value == "mercedes"){
-        document.querySelector("#productPrice3").textContent = 30000;
-    }
-});
-console.log("Hello");
+   let response = await fetch(`http://localhost:5000/data?site=${cUrl}`);
+   response = await response.json();
+   console.log(response);
+
+   document.querySelector("#productName").textContent = response.bkprod;
+
+   document.querySelector("#productImage").src = response.cpic;
+
+   document.querySelector("#productList1").innerHTML = '';
+   document.querySelector("#productList2").innerHTML = '';
+   document.querySelector("#productList3").innerHTML = '';
+
+
+   let count=1;
+
+   if (response.prodn1.length != 0) {
+      let first = count;
+      response.prodn1.forEach((element, index) => {
+         document.querySelector(`#productList${first}`).innerHTML += '<option value="' + index + '" class="truncate">' + element + '</option>'
+      })
+      
+      document.querySelector(`#logo${first}`).src='./image/flipkart.png'
+      document.querySelector(`#productPrice${first}`).textContent = response.price1[0];
+      document.querySelector(`#button${first}`).href = response.url1;
+      document
+      .querySelector(`#productList${first}`)
+      .addEventListener("change", function () {
+         if (this.value == "0") {
+            document.querySelector(`#productPrice${first}`).textContent = response.price1[0];
+         } else if (this.value == "1") {
+            document.querySelector(`#productPrice${first}`).textContent = response.price1[1];
+         } else if (this.value == "2") {
+            document.querySelector(`#productPrice${first}`).textContent = response.price1[2];
+         } else if (this.value == "3") {
+            document.querySelector(`#productPrice${first}`).textContent = response.price1[3];
+         } else if (this.value == "4") {
+            document.querySelector(`#productPrice${first}`).textContent = response.price1[4];
+         }
+      });
+      
+      count += 1;
+   }
+   
+
+   if (response.prodn2.length != 0) {
+      let second = count;
+      response.prodn2.forEach((element, index) => {
+         document.querySelector(`#productList${second}`).innerHTML += '<option value="' + index + '" class="truncate">' + element + '</option>'
+      })
+      document.querySelector(`#logo${second}`).src='./image/amazon.png'
+      document.querySelector(`#productPrice${second}`).textContent = response.price2[0];
+      document.querySelector(`#button${second}`).href = response.url2;
+      document
+      .querySelector(`#productList${second}`)
+      .addEventListener("change", function () {
+         if (this.value == "0") {
+            document.querySelector(`#productPrice${second}`).textContent = response.price2[0];
+         } else if (this.value == "1") {
+            document.querySelector(`#productPrice${second}`).textContent = response.price2[1];
+         } else if (this.value == "2") {
+            document.querySelector(`#productPrice${second}`).textContent = response.price2[2];
+         } else if (this.value == "3") {
+            document.querySelector(`#productPrice${second}`).textContent = response.price2[3];
+         } else if (this.value == "4") {
+            document.querySelector(`#productPrice${second}`).textContent = response.price2[4];
+         }
+      });
+      count += 1;
+   }
+
+   if (response.prodn3.length != 0) {
+      let third = count;
+      response.prodn3.forEach((element, index) => {
+         document.querySelector(`#productList${third}`).innerHTML += '<option value="' + index + '" class="truncate">' + element + '</option>'
+      })
+      document.querySelector(`#logo${third}`).src='./image/croma.png'
+      document.querySelector(`#productPrice${third}`).textContent = response.price3[0];
+      let newurl = response.bkprod.replace(' ', '%20');
+      document.querySelector(`#button${third}`).href = `https://www.croma.com/searchB?q=${newurl}%3Arelevance&text=${newurl}`;
+      document
+      .querySelector(`#productList${third}`)
+      .addEventListener("change", function () {
+         if (this.value == "0") {
+            document.querySelector(`#productPrice${third}`).textContent = response.price3[0];
+         } else if (this.value == "1") {
+            document.querySelector(`#productPrice${third}`).textContent = response.price3[1];
+         } else if (this.value == "2") {
+            document.querySelector(`#productPrice${third}`).textContent = response.price3[2];
+         } else if (this.value == "3") {
+            document.querySelector(`#productPrice${third}`).textContent = response.price3[3];
+         } else if (this.value == "4") {
+            document.querySelector(`#productPrice${third}`).textContent = response.price3[4];
+         }
+      });
+      count += 1;
+   }
+
+   if (response.prodn4.length != 0) {
+      let forth = count;
+      response.prodn4.forEach((element, index) => {
+         document.querySelector(`#productList${forth}`).innerHTML += '<option value="' + index + '">' + element + '</option>'
+      })
+      document.querySelector(`#logo${forth}`).src='./image/reliance.png'
+      document.querySelector(`#productPrice${forth}`).textContent = response.price4[0];
+      document.querySelector(`#button${forth}`).href = response.url4;
+      document
+      .querySelector(`#productList${forth}`)
+      .addEventListener("change", function () {
+         if (this.value == "0") {
+            document.querySelector(`#productPrice${forth}`).textContent = response.price4[0];
+         } else if (this.value == "1") {
+            document.querySelector(`#productPrice${forth}`).textContent = response.price4[1];
+         } else if (this.value == "2") {
+            document.querySelector(`#productPrice${forth}`).textContent = response.price4[2];
+         } else if (this.value == "3") {
+            document.querySelector(`#productPrice${forth}`).textContent = response.price4[3];
+         } else if (this.value == "4") {
+            document.querySelector(`#productPrice${forth}`).textContent = response.price4[4];
+         }
+      });
+   }
+
+   document.querySelector("#loading").style.display = "none";
+
+   document.querySelector("#body").style.display = "block";
+}
+
+main();
---
+++ index1.js
@@ -0,0 +1,5 @@
+let vari = []
+
+//check if vari is empty
+
+console.log(vari.length == 0);
